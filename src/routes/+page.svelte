1da7135aa220cbb311b65b1afa61e8211db968e9
Kevin Wu <kevin@kevinniuwu.com>
2022-12-19 02:21:44 -0500

    Add toolbar, implement word count and download

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,40 @@
-<script>
+<script lang="ts">
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Share, ArrowDownTray, ChartBar } from '@steeze-ui/heroicons';
 	import CodeMirror from 'svelte-codemirror-editor';
 
-	let value = '';
+	function countWords(s: string) {
+		var matches = s.match(/\S+/g);
+		return matches ? matches.length : 0;
+	}
+
+	function countCharactersWithoutSpaces(s: string) {
+		return s.replace(/\s/g, '').length;
+	}
+
+	function downloadFile(title: string, contents: string) {
+		const element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(contents));
+		element.setAttribute('download', title);
+
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	}
+
+	var value = '';
+
+	var wordCount = 0;
+	var characterCount = 0;
+	var characterCountWithoutSpaces = 0;
+	$: {
+		wordCount = countWords(value);
+		characterCount = value.length;
+		characterCountWithoutSpaces = countCharactersWithoutSpaces(value);
+	}
 </script>
 
 <svelte:head>
@@ -16,3 +49,37 @@
 		'.cm-activeLineGutter': { backgroundColor: 'hsl(var(--b3))' }
 	}}
 />
+
+<div class="btn-group absolute right-3 bottom-3">
+	<button class="btn"><Icon src={Share} size="20px" /></button>
+	<button class="btn" on:click={() => downloadFile('scratchpad.txt', value)}
+		><Icon src={ArrowDownTray} size="20px" /></button
+	>
+	<label class="btn" for="word-count-modal"><Icon src={ChartBar} size="20px" /></label>
+</div>
+
+<input type="checkbox" id="word-count-modal" class="modal-toggle" />
+<div class="modal">
+	<div class="modal-box max-w-md">
+		<h3 class="font-bold text-lg">Word count</h3>
+		<table class="table w-full">
+			<tbody>
+				<tr>
+					<td>Words</td>
+					<td class="text-end">{wordCount}</td>
+				</tr>
+				<tr>
+					<td>Characters</td>
+					<td class="text-end">{characterCount}</td>
+				</tr>
+				<tr>
+					<td>Characters excluding spaces</td>
+					<td class="text-end">{characterCountWithoutSpaces}</td>
+				</tr>
+			</tbody>
+		</table>
+		<div class="modal-action">
+			<label for="word-count-modal" class="btn">OK</label>
+		</div>
+	</div>
+</div>
